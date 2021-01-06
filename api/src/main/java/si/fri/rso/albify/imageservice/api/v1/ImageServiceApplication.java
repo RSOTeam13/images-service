6915524ddfe19df6fed4f7bb6bdcd190dbbf43bb
Anze Mur <am3158@student.uni-lj.si>
2021-01-06 01:27:12 +0100

    Fixed filters.

--- api/src/main/java/si/fri/rso/albify/imageservice/api/v1/ImageServiceApplication.java
+++ api/src/main/java/si/fri/rso/albify/imageservice/api/v1/ImageServiceApplication.java
@@ -1,9 +1,23 @@
 package si.fri.rso.albify.imageservice.api.v1;
 
+import si.fri.rso.albify.imageservice.api.v1.resources.ImageResource;
+import si.fri.rso.albify.imageservice.services.filters.AuthenticationFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/v1")
 public class ImageServiceApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> resources = new HashSet<Class<?>>();
+        resources.add(ImageResource.class);
+        resources.add(AuthenticationFilter.class);
+
+        return resources;
+    }
 
 }
